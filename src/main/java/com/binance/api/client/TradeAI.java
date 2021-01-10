c34d473ff10ce615f89e375cb8453b0d1394e073
naonn <jaryen545@gmail.com>
2021-01-10 14:07:40 -0800

    Update

--- src/main/java/com/binance/api/client/TradeAI.java
+++ src/main/java/com/binance/api/client/TradeAI.java
@@ -5,6 +5,16 @@ public class TradeAI {
 
     //EMA
 
+    // For period 21, prevAvg = SMA calculated for last 20 periods
+    // After period 21, prevAvg = previous EMA
+    public double calculateEMA(double currPrice, double prevAvg, double periodNum) {
+        // prevAvg = previous period's SMA or EMA
+        // double k = 0.095238;
+        double k = 2 / (periodNum + 1);
+
+        double EMA = (k / (currPrice - prevAvg)) + prevAvg;
+        return EMA;
+    }
 
     //SMA
     public void add(double price){
