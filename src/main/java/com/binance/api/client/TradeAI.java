cd9b921b7bcd3330d4ef7077b4d5f5fd33ae6a83
Brent Li <44733139+brentli17@users.noreply.github.com>
2021-01-10 13:46:59 -0800

    Create TradeAI.java

---
+++ src/main/java/com/binance/api/client/TradeAI.java
@@ -0,0 +1,17 @@
+package com.binance.api.client;
+
+public class TradeAI {
+    double[] btcPrice;
+
+    //EMA
+
+
+    //SMA
+    public void add(double price){
+        
+    }
+
+    public void average(){
+
+    }
+}
